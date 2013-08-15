50fff46651d8075425829155d4f3149c176adc94
ryan.malone@blondgorilla.com <ryan.malone@blond-pc-02.domain.blondgorilla.com>
2013-08-16 06:06:53 +0800

    First commit, getting started on code first db

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+        //
+        // GET: /HelloWorld/
+
+       
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+ 
+
+        //public string Welcome( string name, int numTimes = 1)
+        //{
+        //   // return "This is the welcome action method...";
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+
+        //}
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+    }
+}
